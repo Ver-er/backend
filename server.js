1cef6b406513f1bca0491b31b8c2641e44669ea5
Ver-er <vishnu0vardhan4@gmail.com>
2025-03-02 16:27:34 +0530

    ``` No code changes made. ```

--- server.js
+++ server.js
@@ -10,40 +10,34 @@ const PORT = 3000
 app.use(express.json());
 
 //Home page api
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send('<h1 align=center>Welcome to the MERN stack week 2 session<h1>')
 })
 
 //Registration page api
-app.post('/register',async(req, res)=>{
-    const {username,email,password} = req.body
-    try{
+app.post('/register', async (req, res) => {
+    const { username, email, password } = req.body
+    try {
         const hashPassword = await bcrypt.hash(password, 10)
-        const user= new Users({username,email,password:hashPassword})
+        const user = new Users({ username, email, password: hashPassword })
         await user.save()
-        res.json({message:"User registered"})
+        res.json({ message: "User registered" })
         console.log('User registered successfully')
-
-    }
-    catch(err)
-    {
+    } catch (err) {
         console.log(err)
     }
 })
 
 //Login page api
-app.post('/login',async(req, res)=>{
-    const {email,password} = req.body
-    try{
-        const user = await Users.findOne({email})
-        if (!user || !(await bcrypt.compare(password, user.password)))
-            {
-            return res.status(400).json({message: "Invalid credentials"})
-            }
-        res.json({message:"User logged in", username:user.username})
-    }
-    catch(err)
-    {
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = await Users.findOne({ email })
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+        res.json({ message: "User logged in", username: user.username })
+    } catch (err) {
         console.log(err)
     }
 })
@@ -62,12 +56,10 @@ app.post('/recipe', async (req, res) => {
     }
 })
 
-mongoose.connect(process.env.MONGO_URL).then(()=>console.log('DB connected successfully...')).catch((err)=>console.log(err))
-
+mongoose.connect(process.env.MONGO_URL).then(() => console.log('DB connected successfully...')).catch((err) => console.log(err))
 
 app.listen(PORT, (err) => {
-    if(err)
-    {
+    if (err) {
         console.log(err)
     }
     console.log(`Server is running on port ${PORT}`)
